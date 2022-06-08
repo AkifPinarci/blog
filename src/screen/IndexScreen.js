7c79c8937832be7db5f221ae233179cab5ca7c56
AkifPinarci <mehmetakifpinarci@gmail.com>
2022-06-07 23:43:01 -0700

    Init ContextAPI tutorial setup

---
+++ src/screen/IndexScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+const IndexScreen = () => {
+  return <Text>IndexScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
